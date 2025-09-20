511bd50cce494e060f570bd1c15c33ecd1773c49
DavideArruzzolo <arruzzolod.lavoro@gmail.com>
2025-09-20 18:31:40 +0200

    UI sound and small fix

--- scripts/ui/Menu.gd
+++ scripts/ui/Menu.gd
@@ -1,14 +1,46 @@
 extends Control
 
+@onready var ui_sound = $AudioStreamPlayer
+
+
+func _ready() -> void:
+	$MenuButtons/StartButton.mouse_entered.connect(_on_button_mouse_entered)
+	$MenuButtons/OptionsButton.mouse_entered.connect(_on_button_mouse_entered)
+	$MenuButtons/ExitButton.mouse_entered.connect(_on_button_mouse_entered)
+
+	var made_by_buttons = [
+		$MadeBy/ProfileHolder/human,
+		$MadeBy/ProfileHolder/human2,
+		$MadeBy/ProfileHolder/human3,
+		$MadeBy/ProfileHolder/human5,
+		$MadeBy/ProfileHolder/human6
+	]
+
+	for button in made_by_buttons:
+		button.mouse_entered.connect(_on_button_mouse_entered)
+		button.pressed.connect(Callable(self, "_on_made_by_button_pressed").bind(button.uri))
+
 
 func _on_start_button_pressed() -> void:
+	ui_sound.play()
 	GameManager.start_game()
 
 
 func _on_options_button_pressed() -> void:
+	ui_sound.play()
 	visible = false
 	get_tree().change_scene_to_file.call_deferred("res://scenes/ui/OptionsMenu.tscn")
 
 
 func _on_exit_button_pressed() -> void:
+	ui_sound.play()
 	get_tree().quit()
+
+
+func _on_made_by_button_pressed(uri: String) -> void:
+	ui_sound.play()
+	OS.shell_open(uri)
+
+
+func _on_button_mouse_entered() -> void:
+	ui_sound.play()
--- scripts/ui/Options.gd
+++ scripts/ui/Options.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var slider = $MenuButtons/HSlider
+@onready var ui_sound = $AudioStreamPlayer
 
 const MIN_VOLUME_DB = -80.0
 const MAX_VOLUME_DB = 0.0
@@ -8,9 +9,23 @@ const MAX_VOLUME_DB = 0.0
 
 func _ready() -> void:
 	$MenuButtons/HSlider.value = 5 # Initial slider value at 5%
+	$MenuButtons/TextureButton.mouse_entered.connect(_on_button_mouse_entered)
+
+	var made_by_buttons = [
+		$MadeBy/ProfileHolder/human,
+		$MadeBy/ProfileHolder/human2,
+		$MadeBy/ProfileHolder/human3,
+		$MadeBy/ProfileHolder/human5,
+		$MadeBy/ProfileHolder/human6
+	]
+
+	for button in made_by_buttons:
+		button.mouse_entered.connect(_on_button_mouse_entered)
+		button.pressed.connect(Callable(self, "_on_made_by_button_pressed").bind(button.uri))
 
 
 func _on_h_slider_value_changed(slider_value: float):
+	ui_sound.play()
 	if slider_value == 0:
 		BackgroundMusic.volume_db = MIN_VOLUME_DB
 		BackgroundMusic.stream_paused = true
@@ -20,5 +35,15 @@ func _on_h_slider_value_changed(slider_value: float):
 
 
 func _on_back_button_pressed() -> void:
+	ui_sound.play()
 	visible = false
 	get_tree().change_scene_to_file.call_deferred("res://scenes/ui/StartMenu.tscn")
+
+
+func _on_made_by_button_pressed(uri: String) -> void:
+	ui_sound.play()
+	OS.shell_open(uri)
+
+
+func _on_button_mouse_entered() -> void:
+	ui_sound.play()
