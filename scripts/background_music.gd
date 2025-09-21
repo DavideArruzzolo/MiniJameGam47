f26bf14910cfe51927f955a2e1bcac6432d884f4
DavideArruzzolo <arruzzolod.lavoro@gmail.com>
2025-09-21 20:07:24 +0200

    Music

--- scripts/background_music.gd
+++ scripts/background_music.gd
@@ -1,5 +1,10 @@
 extends AudioStreamPlayer
 
 func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
 	if not is_playing():
 		play()
+	if stream:
+		var playback = get_stream_playback()
+		if playback and playback.has_method("set_loop"):
+			playback.set_loop(true)
