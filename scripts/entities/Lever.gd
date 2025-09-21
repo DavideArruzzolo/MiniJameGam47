1ad75b7c372a22848cbb99d3b4177f1671c39ff1
DavideArruzzolo <arruzzolod.lavoro@gmail.com>
2025-09-21 12:25:20 +0200

    Tutorial level

--- scripts/entities/Lever.gd
+++ scripts/entities/Lever.gd
@@ -1,6 +1,9 @@
 extends Switchable
 class_name Lever
 
+@export var tile_map_node: TileMap
+@export var cells_to_erase: Array[Vector2i]
+@export var tile_map_layer_index: int = 0
 @export var allowed_clone_type: String = ""
 @export var egg_sprite_texture: Texture2D
 
@@ -21,6 +24,16 @@ func update_egg_sprite():
 		egg_sprite_node.visible = false
 
 func onInteract():
+	var current_player = PlayerManager.players[PlayerManager.current_player_index]
+	var can_interact = false
+	if allowed_clone_type == "":
+		can_interact = true
+	elif current_player.name == allowed_clone_type:
+		can_interact = true
+
+	if not can_interact:
+		return
+
 	if Status:
 		Status = false
 		$AnimatedSprite2D.play("default")
@@ -29,25 +42,17 @@ func onInteract():
 		Status = true
 		$AnimatedSprite2D.play("default", -1, true)
 		$AudioStreamPlayer.play()
+		
+		if tile_map_node and not cells_to_erase.is_empty():
+			call_deferred("erase_cells_with_autotile", cells_to_erase)
 
 func onBodyEntered(body: Node2D) -> void:
 	if body is Player:
-		var can_interact = false
-		if allowed_clone_type == "":
-			can_interact = true
-		elif body.name == allowed_clone_type:
-			can_interact = true
-
-		if can_interact:
-			body.interacted_event.connect(onInteract)
+		body.interacted_event.connect(onInteract)
 
 func onBodyExited(body: Node2D) -> void:
 	if body is Player:
-		var can_interact = false
-		if allowed_clone_type == "":
-			can_interact = true
-		elif body.name == allowed_clone_type:
-			can_interact = true
+		body.interacted_event.disconnect(onInteract)
 
-		if can_interact:
-			body.interacted_event.disconnect(onInteract)
+func erase_cells_with_autotile(cells: Array[Vector2i]) -> void:
+	tile_map_node.set_cells_terrain_connect(tile_map_layer_index, cells, 0, 1)
